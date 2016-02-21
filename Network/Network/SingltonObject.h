418439929ee1e2ca1552e2f9c5b2bd31d09b6a08
lucky1one <whitehany@naver.com>
2016-02-22 06:54:29 +0900

    network

---
+++ Network/Network/SingltonObject.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "header.h"
+#include "ProactorService.h"
+#include "ProactorIDMap.h"
+
+typedef ACE_Singleton<ProactorServiceIDMap<ACE_Null_Mutex, ProactorService, 5000>, ACE_Thread_Mutex> ProactorServiceMapSingleton;
